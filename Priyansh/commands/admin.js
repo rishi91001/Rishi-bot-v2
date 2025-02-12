6497a3456529e44591708b91bd27a53714406303
rishi91001 <rishikushwahag106@gmail.com>
2025-02-12 12:46:49 +0530

    Update admin.js

--- Priyansh/commands/admin.js
+++ Priyansh/commands/admin.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "admin",
 	version: "1.0.5",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𓆩 ͜͡ऋषि᭄ ͜͡कुश ͜͡वाहा ͜͡जी𓆪",
 	description: "Admin Config",
 	commandCategory: "Admin",
 	usages: "Admin",
